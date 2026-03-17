3e70dd1dba50f8b13ce951910f2146668b2e4711
Hariom <hbharti156@gmail.com>
2026-03-17 16:07:47 +0530

    route

--- src/app.js
+++ src/app.js
@@ -3,12 +3,31 @@ const express = require('express');
 const app = express();
 
 
-const port  = 3000;
+// const port  = 3000;
+// this will only match the get http method api call to /user
+app.get("/user",(req,res)=>{
+    res.send({fmane: "mahesh", lname:"bharti"})
+})
+
+// this will match all the http method api call to /test
 
 app.use('/test',(req,res)=>{
     res.send("hello from the server!!!1 ")
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on port: ${port}`);
+// app.use('/hello/2',(req,res)=>{
+//     res.send("jai sharee raamm")
+// })
+
+
+// app.use('/hello',(req,res)=>{
+//     res.send("hello hellllllooo mahesh")
+// })
+
+// app.use('/',(req,res)=>{
+//     res.send("hello mahesh bharti ")
+// })
+
+app.listen(3000, ()=>{
+    console.log(`server is running on port: 3000`);
 });
